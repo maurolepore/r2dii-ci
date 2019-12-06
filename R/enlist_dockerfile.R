ed5e98a7bab73872025ec53681f434342858397e
Mauro Lepore <maurolepore@gmail.com>
2019-12-06 15:22:05 -0600

    New create_dockerfile()

--- R/enlist_dockerfile.R
+++ R/enlist_dockerfile.R
@@ -6,10 +6,9 @@
 #' @export
 #'
 #' @examples
-#' r_version <- c("3.6.0", "3.5.0")
-#' docker_files <- enlist_dockerfiles(r_version)
-#' docker_files
-enlist_dockerfiles <- function(r_version) {
+#' r_version <- glue::glue("3.{6:2}.0")
+#' enlist_dockerfile(r_version)
+enlist_dockerfile <- function(r_version) {
   path <- system.file("Dockerfile", package = "r2dii.ci")
   docker_file <- readLines(path)
   from_image <- sprintf(
@@ -19,4 +18,26 @@ enlist_dockerfiles <- function(r_version) {
 
   out <- lapply(from_image, function(x) c(x, docker_file))
   stats::setNames(out, r_version)
+}
+
+#' Create dockerfiles for a given R version
+#'
+#' @inherit enlist_dockerfile
+#'
+#' @return Called for it's side effect (returns the first argument invisibly).
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' r_version <- c("3.6.0", "3.5.0")
+#' create_dockerfile(r_version)
+#' }
+create_dockerfile <- function(r_version) {
+  docker_files <- enlist_dockerfile(r_version)
+  out_path <- fs::path(r_version, "Dockerfile")
+
+  fs::dir_create(fs::path_dir(out_path))
+  purrr::walk2(docker_files, out_path, writeLines)
+
+  invisible(r_version)
 }
